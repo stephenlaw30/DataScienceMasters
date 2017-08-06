6b307ebbee1f60ab29efdeef6b668e52f33b0a2a
Nimz <s.newns92@gmail.com>
2017-08-05 21:46:04 -0400

    Continue with Wk 6 for Coursera Stanford ML

--- Stats/ISL/Ch2_StatisticalLearning/ISL_ch2.R
+++ Stats/ISL/Ch2_StatisticalLearning/ISL_ch2.R
@@ -1,16 +1,7 @@
-setwd('C:/Users/NEWNSS/Dropbox/DataScienceMasters/Stats/ISL/Ch2_StatisticalLearning')
+setwd('C:/Users/Nimz/Dropbox/DataScienceMasters/Stats/ISL/Ch2_StatisticalLearning')
 
-'Suppose we are statistical consultants hired by a client to provide advice on how to improve sales of a product. 
-  The dataset contains sales (in thousands of units) of a product over 200 different markets w/ advertising budgets
-  for different channels/media.
-
-It is not possible for our client to directly increase sales of the product. On the other hand, they can control
-the advertising expenditure in each of the 3 channels. Therefore, if we determine that there is an association 
-between advertising and sales, we can instruct our client to adjust advertising budgets, thereby indirectly 
-increasing sales.
-
-In other words, our goal is to develop an accurate modelthat can be used to predict sales on the basis of the 
-3 channels budgets.'
+## develop an accurate modelthat can be used to predict sales on the basis of the different 
+##    channels' advertising budgets.'
 
 ads <- read.csv('Advertising.csv')
 head(ads)
@@ -27,9 +18,30 @@ glimpse(ads)
 We are estimating Y based on given X values/points. The errors in our predictions vs. actual values should have
    a mean = 0.
 
-statistical learning refers to a set of approaches for estimating f.'
+Statistical learning refers to a set of approaches for estimating f.'
 
 tv_sales <- ggplot(ads) + geom_point(aes(TV,Sales)) # most condensed plot
 nwsppr_sales <- ggplot(ads) + geom_point(aes(Newspaper,Sales)) # most scattered plot
 radio_sales <- ggplot(ads) + geom_point(aes(Radio,Sales))
 
+'2 main reasons to estimate f:
+  - prediction
+    - may have x values readily available, but cannot easily obtain y values
+    - since E averages to 0, we can predict with w/ y_hat = f_hat(x)
+    - f_hat is usually a black box, provided it yields accurate predictions for y
+    - accuracy of y_hat in relation to y depends on REDUCIBLE ERROR and IRREDUCIBLE ERROR
+    - f_hat will generally not be a perfect estimate of f, and the error in this estimate 
+        is REDUCIBLE
+    - We can potentially improve the accuracy of f_hat via the most appropriate statisical
+        learning technique
+    - Even if we found the "perfect" estimate, we would still have error because Y is also
+         a function of E, which cannot be predicted with X, by definition
+    - Therefore variability in E also affects prediction accuracy, and is the IRREDUCIBLE 
+        error
+    - E may contain unmeasures variables useful in predicting Y, and since we do not 
+      measure them, we cannot use them in f to predict Y
+    - E may also carry unmeasurable variation (variation in drug manufacturing or in how
+         a patient is feeling may vary the risk of an adverse reaction)
+    - E(Y - Y_hat)^2
+  - inference
+'
