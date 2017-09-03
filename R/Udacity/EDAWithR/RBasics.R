0c26891b17d5cdc8cb85dbc7c6248ec53e05b175
Nimz <s.newns92@gmail.com>
2017-09-03 15:36:16 -0400

    Finish Lesson 2 in EDA with R from Udacity

--- R/Udacity/EDAWithR/RBasics.R
+++ R/Udacity/EDAWithR/RBasics.R
@@ -14,3 +14,15 @@ ggplot(reddit) + geom_bar(aes(income.range))
 # possible ORDERED FACTORS = age.range, income range
 
 # set levels of ordered factor age.range
+reddit$age.range <- factor(reddit$age.range, levels = c('Under 18', '18-24', '25-34', '35-44', '45-54', '55-64', '65 or Above'),
+                           ordered = T)
+
+ggplot(reddit) + geom_bar(aes(age.range))
+
+# reorder income range level
+table(reddit$income.range)
+reddit$income.range <- factor(reddit$income.range, levels = c('Under $20,000', '$20,000 - $29,999', '$30,000 - $39,999', 
+                                                              '$40,000 - $49,999', '$50,000 - $69,999', '$70,000 - $99,999',
+                                                              '$100,000 - $149,999', '$150,000 or more'), ordered = T)
+
+ggplot(reddit) + geom_bar(aes(income.range))
