8acec39ba0b26a508c8bc2a2cbd403cd276c879d
Newns <s.newns92@gmail.com>
2017-09-07 15:03:13 -0400

    Work up to logical variable videos in Exploring 1 Variable in Udacity EDA with R

--- R/Udacity/EDAWithR/lesson3_ExploreOneVariable.R
+++ R/Udacity/EDAWithR/lesson3_ExploreOneVariable.R
@@ -1,4 +1,4 @@
-setwd('C:/Users/Nimz/Dropbox/DataScienceMasters/R/Udacity/EDAWithR')
+setwd('C:/Users/NEWNSS/Dropbox/DataScienceMasters/R/Udacity/EDAWithR')
 # check wd for files
 list.files()
 
@@ -80,7 +80,7 @@ ggplot(subset(facebook, !is.na(gender))) +
   facet_wrap(~ gender)
 
 qplot(x = friend_count, data = subset(facebook, !is.na(gender)), 
-      xlim = c(0,1000), binwidth = 25) + 
+      binwidth = 25) + 
   scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50)) +
   facet_wrap(~ gender)
 
@@ -138,3 +138,198 @@ ggplot(facebook) +
   scale_x_continuous(breaks = seq(0,113,5)) + 
   xlab('Age') +
   ylab('Count')
+
+'******************************************************************************************************'
+                        'ENGAGEMENT VARIABLES + TRANSFORMING DATA'
+'******************************************************************************************************'
+# some people have ORDERS OF MAGNITUDE more likes, comments, shares, etc. than any other users
+# this means the data is OVER-DISPERSED (very-long tailed)
+# helps to transform these values to see SD's or orders of magnitude so that we can shorten the tail
+
+# shorten friend count tail w/ nlog transform
+ggplot(facebook) + 
+  geom_histogram(aes(log(friend_count)), color = 'white')
+# this is base 10 by default
+# many users have 0 friends, so base 10 is not defined for 0 (-Inf), so we see an error for 1962 rows
+# looks much more normal = better for common statistical technique that assume this distribution
+
+# specify base 10 + add 1 to result to remove errors
+ggplot(facebook) + 
+  geom_histogram(aes(log10(friend_count+1)), color = 'orange')
+# same plot shape, different x-axis
+# comparing friend counts on orders of magnitude of 10 when doing log10 (10 pt scale)
+
+summary(log10(facebook$friend_count+1))
+
+# show 3 histograms of friend count = original, log10 transform, and sqrt transform
+
+library(gridExtra) # multiple plots on 1 graph
+p1 = ggplot(facebook) + 
+  geom_histogram(aes(friend_count), color = 'white')
+p2 = ggplot(facebook) + 
+  geom_histogram(aes(log10(friend_count+1)), color = 'green')
+p3 = ggplot(facebook) + 
+  geom_histogram(aes(sqrt(friend_count)), color = 'red')
+grid.arrange(p1,p2,p3)
+# sqrt is better than no transform at all, but log10 is best bc most normal
+
+# ALTERNATE SOLUTION
+p1 = ggplot(facebook) + 
+  geom_histogram(aes(friend_count), color = 'white')
+# use previous plot + add scale the x-axis using log10
+p2 = p1 + 
+  scale_x_log10()
+# use 1st plot + add scale the x-axis using sqrt
+p3 = p1 + 
+  scale_x_sqrt()
+grid.arrange(p1,p2,p3)
+
+# Notice a different x-scale on this 2nd method
+logScale <- ggplot(facebook) + 
+  geom_histogram(aes(log10(friend_count+1)), color = 'green')
+
+countScale <- ggplot(facebook) + 
+  geom_histogram(aes(friend_count), color = 'white') + 
+  scale_x_log10()
+
+grid.arrange(logScale, countScale)
+
+# scale_x_log10 shows x-axis is actual friend counts, log10() wrapper shows it in log units
+# just keep this in mind when making plots 
+# might be easier to think in terms of actual counts w/ scale layer
+
+'******************************************************************************************************'
+                            'FREQUENCY POLYGON'
+'******************************************************************************************************'
+# this another plot that lets us compare distributions
+# similar to histograms in but via a curve that connect counts in a histogram
+# can see shapes + peaks in distributions in more detail
+
+# remake the gender friend count comparison via a frequency polygram
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = friend_count), binwidth = 25) + 
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0,1000,50)) +
+  facet_wrap(~ gender)
+
+'qplot(x = friend_count, data = subset(facebook, !is.na(gender)), 
+      binwidth = 25, geom = \'freqpoly\') + 
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50)) +
+  facet_wrap(~ gender)'
+
+# make 1 plot but use color to show different genders
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = friend_count, color = gender), binwidth = 25) + 
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0,1000,50))
+
+'qplot(x = friend_count, data = subset(facebook, !is.na(gender)), 
+      binwidth = 25, geom = \'freqpoly\', color = gender) + 
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))'
+
+# can now compare 2+ distributions at once on same graph
+# but still doesn't answer question of who has more friends on average
+# change y-axis to proportions instead of counts
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = friend_count, y = ..count../sum(..count..), 
+                    color = gender), binwidth = 10) + 
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0,1000,50)) + 
+  xlab('Friend Count') + 
+  ylab('Proportion of Users with that friend count')
+
+'qplot(x = friend_count, data = subset(facebook, !is.na(gender)), y = ..count../sum(..count..),  
+      binwidth = 10, geom = \'freqpoly\', color = gender) + 
+scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50))'
+
+# can now see a higher proportion of males have lower friend counts
+# females most likely overtake males in the tail end of the graph
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = friend_count, y = ..count../sum(..count..), 
+                    color = gender), binwidth = 10) + 
+  scale_x_continuous(limits = c(450, 1000), breaks = seq(450,1000,50)) + 
+  xlab('Friend Count') + 
+  ylab('Proportion of Users with that friend count')
+
+
+'***********************which gender creates more traffic on the web?********************************'
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = www_likes, 
+                    color = gender))
+
+# transform
+'ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = log10(www_likes+1), 
+                    color = gender))'
+
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = www_likes, 
+                    color = gender)) + 
+  scale_x_log10()
+
+#proportions
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_freqpoly(aes(x = www_likes, y = ..count../sum(..count..),
+                    color = gender)) + 
+  scale_x_log10()
+
+# females have more on average, and more larger proportion of males direct less traffic
+# let's try numerical statistics to dive into this more
+
+c('male likes: ', sum(subset(facebook,gender == 'male')$www_likes), 
+  'female likes: ', sum(subset(facebook,gender == 'female')$www_likes))
+# so females have more overall likes, almost 2x
+
+# ALTERNATE
+# apply a function (sum) to the www traffic split by factors of gender
+by(facebook$www_likes, facebook$gender, sum)
+
+'******************************************************************************************************'
+                                      'BOX PLOTS'
+'******************************************************************************************************'
+# also helpful for viewing distributions
+
+# view friend count by gender --> categorical variable (grouping) = x, continous variable = y
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friend_count))
+
+# see so many outliers, show just the boxes (< 100 friends)
+ggplot(subset(subset(facebook, !is.na(gender)), friend_count < 1000)) +
+  geom_boxplot(aes(gender, friend_count))
+
+# alternate
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friend_count)) +
+  ylim(0,1000)
+
+# alternate 2
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friend_count)) +
+  scale_y_continuous(limits = c(0,1000))
+# females seem to have a higher median, but still not accurate bc we're removing rows (see output msg)
+
+# alternate 3 w/out removing rows
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friend_count)) +
+  coord_cartesian(ylim = c(0,1000))
+# females seem to have a higher median of friends (higher on "average"), and tops of boxes seems higher
+# different isn't very large in average # of friends
+
+# check middle 50% (box)
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friend_count)) +
+  coord_cartesian(ylim = c(0,250))
+# medians seem more similar now
+
+# check quartiles (75% of females have friend counts < 244)
+# or 25% of female users have > 244 friends
+by(facebook$friend_count, facebook$gender, summary)
+# males have smaller box = less variable
+
+# on average, who initiated more friendships?
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friendships_initiated))
+
+ggplot(subset(facebook, !is.na(gender))) +
+  geom_boxplot(aes(gender, friendships_initiated)) + 
+  coord_cartesian(ylim = c(0,200))
+
+by(facebook$friendships_initiated, facebook$gender, summary)
+# females barely
