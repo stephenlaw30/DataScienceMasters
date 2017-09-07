d892686074fdf374a1245cc2161e8e3c4528b8df
Nimz <s.newns92@gmail.com>
2017-09-06 20:03:02 -0400

    Work more through EDAExploring 1 Variable in EDA with R from Udacity

--- R/Udacity/EDAWithR/lesson3_ExploreOneVariable.R
+++ R/Udacity/EDAWithR/lesson3_ExploreOneVariable.R
@@ -1,3 +1,4 @@
+setwd('C:/Users/Nimz/Dropbox/DataScienceMasters/R/Udacity/EDAWithR')
 # check wd for files
 list.files()
 
@@ -71,4 +72,69 @@ ggplot(facebook) +
 qplot(x = friend_count, data = facebook, xlim = c(0,1000), binwidth = 25) + 
   scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50)) +
   facet_wrap(~ gender)
-# see the NA's
+
+# see the NA's + remove them by subsetting the dataset to those records that are not NA
+ggplot(subset(facebook, !is.na(gender))) + 
+  geom_histogram(aes(x = friend_count), binwidth = 25) + 
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0,1000,50)) +
+  facet_wrap(~ gender)
+
+qplot(x = friend_count, data = subset(facebook, !is.na(gender)), 
+      xlim = c(0,1000), binwidth = 25) + 
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0,1000,50)) +
+  facet_wrap(~ gender)
+
+# can't really tell which gender has more friends on average
+# get a table to see if there are more males or females
+table(facebook$gender)
+
+# a bit more males
+
+# apply a function (mean friends) to the facebook dataset that is split by factors of gender
+by(facebook$friend_count, facebook$gender, mean)
+# females have more friends on average
+
+# see more summary stats
+by(facebook$friend_count, facebook$gender, summary)
+
+# should use median bc these distributions are skewed, but median is still higher for female (resistant to extremes)
+
+# to better understand users a bit more --> examine tenure (how many days using FB)
+ggplot(facebook) + 
+  geom_histogram(aes(x = tenure), color = 'black', fill = I('#099DD9'), binwidth = 30)
+
+#qplot(x = tenure, data = facebook, bindwidth = 30, color = I('black'), fill = I('#099DD9'))
+
+# can see this in years rather than days
+ggplot(facebook) + 
+  geom_histogram(aes(x = tenure/365), binwidth = 0.25, color = 'black', fill = I('#099DD9'))
+#qplot(x = tenure/365, data = facebook, bindwidth = 0.25, color = I('black'), fill = I('#099DD9'))
+
+
+#seems likes most users on facebook have been on less than 2/5 years
+
+# set x-axis to 1 year jumps
+ggplot(facebook) + 
+  geom_histogram(aes(x = tenure/365), binwidth = 0.25, color = 'black', fill = I('#099DD9')) + 
+  scale_x_continuous(breaks = seq(1,7,1), limits = c(0,7)) + 
+  xlab('Number of Years on Facebook') +
+  ylab('Count')
+#qplot(x = tenure, data = facebook, bindwidth = 30, color = I('black'), fill = I('#099DD9')) + 
+# scale_x_continuous(breaks = seq(1,7,1), limits = c(0,7)) 
+
+
+# create a histograms by age
+ggplot(facebook) + 
+  geom_histogram(aes(x = age), binwidth = 1, color = 'black', fill = I('#099DD9')) + 
+  xlab('Age') +
+  ylab('Count')
+# looks like rollercoaster, most under age of 30
+# can see unusual spikes in mid-late 20's, w/ no users < 13, and odd spikes above 100 (probably fake/mistakes)
+# user must be at least 13 so that makes sense
+
+# make same plot but from 0 to 113 by 5 years (max age in dataset)
+ggplot(facebook) + 
+  geom_histogram(aes(x = age), binwidth = 1, color = 'black', fill = I('#099DD9')) + 
+  scale_x_continuous(breaks = seq(0,113,5)) + 
+  xlab('Age') +
+  ylab('Count')
