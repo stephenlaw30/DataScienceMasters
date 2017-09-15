430413a93fba7d66bcecef75fdbebbe65eb78061
Newns <s.newns92@gmail.com>
2017-09-15 15:32:49 -0400

    Work up to 13.9 Checking the normality of a sample in LSR

--- Stats/LSR/Chapter13_Comparing2Means.R
+++ Stats/LSR/Chapter13_Comparing2Means.R
@@ -52,3 +52,55 @@ independentSamplesTTest(grade ~ tutor, harpo, var.equal = T)
 independentSamplesTTest(grade ~ tutor, harpo, var.equal = F)
 
 chico <- read.csv("chico.Rdata")
+
+'**********************************************************************************************'
+'1-sample t-test'
+'**********************************************************************************************'
+# one-sided t-test --> interested if true mean was > 67.5
+oneSampleTTest(grades, mu = 67.5, one.sided = "greater")
+
+# interested if group A's scores were higher than group B's
+independentSamplesTTest(
+  formula = grade ~ tutor,
+  data = harpo,
+  one.sided = "Anastasia"
+)
+
+'pairedSamplesTTest(
+  ~ grade_test2 + grade_test1,
+  data = chico,
+  one.sided = "grade_test2"
+)'
+
+
+# use t-test function to do 1 sample t-test
+t.test(grades, mu = 67.5)
+
+# welch's independent samples
+t.test(grade ~ tutor, harpo)
+
+# student's independent samples
+t.test(grade ~ tutor, harpo, var.equal = T)
+
+# paired samples t-test
+t.test(x = chico$grade_test2, y = chico$grade_test1, # variable 1 = "test2" scores, variable 2 = "test1" scores
+          paired = TRUE) # paired test
+
+# Compute an effect size for the data from class:
+cohensD(grades, 67.5) # compare students grades mean to hypothesized population mean
+
+# compute Cohen's D in long format
+(mean(grades) - 67.5) / sd(grades)
+'psych students are achieving grades (mean = 72.3%) that are about .5 SDs higher than the level you'd 
+  expect (67.5%) if they were performing at the same level as other students. 
+    - Judged against Cohen's rough guide, this is a MODERATE effect size.'
+
+# compute effect size for Student t test (outcome ~ group)
+cohensD(grade ~ tutor, harpo, method = "pooled")
+
+# compute effect size for Welch's t-test
+cohensD(grade ~ tutor, harpo, method = "unequal")
+'version of Cohen's d reported by independentSamplesTTest() whenever it runs a Welch t-test'
+
+ 
+         
