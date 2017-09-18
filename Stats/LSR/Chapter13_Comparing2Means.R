1abe8a02caae060ac4f4aede40c39ad7260b53ab
Nimz <s.newns92@gmail.com>
2017-09-17 20:09:02 -0400

    Finish Ch 13.9 in LSR

--- Stats/LSR/Chapter13_Comparing2Means.R
+++ Stats/LSR/Chapter13_Comparing2Means.R
@@ -91,16 +91,33 @@ cohensD(grades, 67.5) # compare students grades mean to hypothesized population 
 
 # compute Cohen's D in long format
 (mean(grades) - 67.5) / sd(grades)
-'psych students are achieving grades (mean = 72.3%) that are about .5 SDs higher than the level you'd 
+'psych students are achieving grades (mean = 72.3%) that are about .5 SDs higher than the level you`d 
   expect (67.5%) if they were performing at the same level as other students. 
-    - Judged against Cohen's rough guide, this is a MODERATE effect size.'
+    - Judged against Cohen`s rough guide, this is a MODERATE effect size.'
 
 # compute effect size for Student t test (outcome ~ group)
 cohensD(grade ~ tutor, harpo, method = "pooled")
 
 # compute effect size for Welch's t-test
 cohensD(grade ~ tutor, harpo, method = "unequal")
-'version of Cohen's d reported by independentSamplesTTest() whenever it runs a Welch t-test'
+'version of Cohen`s d reported by independentSamplesTTest() whenever it runs a Welch t-test'
 
- 
-         
+'**********************************************************************************************
+Checking Normality
+**********************************************************************************************' 
+# generate N = 100 normally distributed numbers
+data <- rnorm(n = 100)
+
+# histogram of sample
+hist(data)
+
+# qq plot
+qqnorm(data)
+
+
+'**********************************************************************************************
+Shapiro-Wilk
+**********************************************************************************************' 
+# normal data
+shapiro.test(data)
+# w = high = not a sig departure from normality p > .001
