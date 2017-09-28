116d7ca19664181aca1059be8e3b211cf2b8d9c0
Newns <s.newns92@gmail.com>
2017-09-28 14:36:05 -0400

    Remove plyr from being loaded at same time as dplyr so I can actually summarize, figure out new way to recode income level as needed

--- Stats/Coursera/DukeStatsWithR/Course1_IntroToProb/final/eda.R
+++ Stats/Coursera/DukeStatsWithR/Course1_IntroToProb/final/eda.R
@@ -1,7 +1,7 @@
 load("C:/Users/NEWNSS/Dropbox/brfss2013.RData")
 who()
 
-library(plyr)
+#library(plyr)
 library(tidyverse)
 library(ggplot2)
 library(maps)
@@ -59,7 +59,6 @@ top_every_day_smoker_states <- smokers %>%
 
 '***********HEAT MAP************************'
 top_every_day_smoker_states$region <- tolower(top_every_day_smoker_states$X_state)
-library(mapproj)
 
 states <- map_data("state")
 map.df <- merge(states,top_every_day_smoker_states, by="region", all.x=T)
@@ -85,7 +84,7 @@ top_every_day_smoker_states %>%
 top_every_day_states <- top_every_day_smoker_states %>%
   head(15) %>% 
   select(X_state) %>%
-  as.data.frame %>%
+  as.data.frame() #%>%
 
 top_every_day_states <- top_every_day_states[,1]
   
@@ -100,11 +99,10 @@ smokers %>%
   filter(X_state %in% top_every_day_states) %>%
   ggplot() + 
     geom_bar(aes(X_state, prop, fill = smokday2), stat = "identity") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))'' + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
     xlab("State") + 
     ylab("Proportion") + 
     ggtitle("Proportion of Every Day Smokers Over All Smokers")
-
 
 
 '****************************************college grads'
@@ -113,27 +111,66 @@ brfss2013 %>%
   filter(educa == "College 4 years or more (College graduate)") %>%
   count
 
-# calculate chance of having income > mean income for college grads
-# overall prob of having 70+ power-user friends
-sum(?dbinom(70:n,n,prob_success))
-
 college_grads_not_retired <- brfss2013 %>%
   filter(educa == "College 4 years or more (College graduate)",
          employ1 %in% c("Employed for wages","Self-employed"))
-table(college_grads_not_retired$income2)
+#table(college_grads_not_retired$income2)
 
 # rename factors to numerics
-college_grads_not_retired$incomeRank <- mapvalues(college_grads_not_retired$income2, from = c("Less than $10,000","Less than $15,000",
-                                                                                "Less than $20,000","Less than $25,000","Less than $35,000",
-                                                                                "Less than $50,000","Less than $75,000","$75,000 or more"), 
-                                            to = c(1, 2, 3, 4, 5, 6, 7, 8))
+college_grads_not_retired$incomeRank <- recode(college_grads_not_retired$income2, "Less than $10,000" = 1, "Less than $15,000" = 2,
+                                                                                "Less than $20,000" = 3, "Less than $25,000" = 4,
+                                                                                "Less than $35,000" = 5, "Less than $50,000" = 6,
+                                                                                "Less than $75,000" = 7,"$75,000 or more" = 8)
 ggplot(college_grads_not_retired) + 
   geom_histogram(aes(as.numeric(incomeRank)), binwidth = 1)
 
 # percent of people making more than 50k
 college_grads_not_retired %>%
-  group_by(income2) %>%
   summarize(prop = mean(as.numeric(incomeRank) > 6, na.rm = T))
-#0.7699314 = 76%
+#0.7699314 = 77%
 
-'****************************************college grads'
+'****************************************p[hys mentakl health veggies and exercise'
+table(brfss2013$physhlth)
+table(brfss2013$menthlth)
+
+healthy <- brfss2013 %>%
+  filter(physhlth == 0 & menthlth == 0 & !(is.na(physhlth)) & !(is.na(menthlth)))
+
+non_healthy_prelim <- brfss2013 %>%
+  filter(physhlth %not in% c(0, 60) & menthlth %not in% c(0,247,5000) & !(is.na(physhlth)) & !(is.na(menthlth)))
+
+summary(non_healthy_prelim$physhlth)
+hist(non_healthy_prelim$physhlth)
+
+summary(non_healthy_prelim$menthlth)
+hist(non_healthy_prelim$menthlth)
+
+non_healthy_prelim2 <- non_healthy_prelim %>%
+  filter(physhlth > 15 & menthlth > 15)
+
+summary(non_healthy_prelim2$physhlth)
+hist(non_healthy_prelim2$physhlth)
+
+summary(non_healthy_prelim2$menthlth)
+hist(non_healthy_prelim2$menthlth)
+
+prop.table(table(healthy$exerany2)) #many healthy exercises
+
+prop.table(table(non_healthy_prelim2$exerany2)) # more did not exercise
+
+
+healthy %>%
+  filter(exerany2 == "Yes") %>%
+  group_by(exeroft1) %>%
+  summarize(count = n()) %>%
+  mutate(prop = count / sum(count)) %>%
+  arrange(desc(prop)) %>%
+  #head(10) %>%
+  #filter(X_state %in% top_every_day_states) %>%
+  ggplot() + 
+  geom_bar(aes(X_state, prop, fill = smokday2), stat = "identity") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))'' + 
+  xlab("State") + 
+  ylab("Proportion") + 
+  ggtitle("Proportion of Every Day Smokers Over All Smokers")
+
