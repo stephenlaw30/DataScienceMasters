5b6a1851ded085647b5baed2f5e34cc6fcaedeec
Nimz <s.newns92@gmail.com>
2017-10-12 06:29:32 -0400

    Finish Logistic Regression in R for Udemy MLAZ

--- MachineLearning/Udemy/08_Logistic_Regression/Logistic Regression.R
+++ MachineLearning/Udemy/08_Logistic_Regression/Logistic Regression.R
@@ -15,10 +15,9 @@ social network collected data on the users and whether or not the bought the SUV
 We will only use age and salary to predict this'
 
 (social <- social %>%
-  select(Age,EstimatedSalary,Purchased))
+   select(Age,EstimatedSalary,Purchased))
 
 # splitting data
-library(caTools)
 set.seed(123)
 
 spl <- sample.split(social$Purchased, SplitRatio = .75)
@@ -39,7 +38,7 @@ test <- test %>%
 head(test)
 
 ## fit to training data
-log.reg <- lm(Purchased ~ ., training, family = "binomial") # binomial regression (1 or 0, yes or no, etc.)
+log.reg <- glm(Purchased ~ ., training, family = "binomial") # binomial regression (1 or 0, yes or no, etc.)
 summary(log.reg)
 
 # Both age and salary are "good" predictors of purchased, but our R2 is quite low
@@ -60,15 +59,15 @@ table(test$Purchased,y.pred)
 fpr <- 7/(25+7) # FP / (FP + TP)
 fnr <- 11/(11+57) # FN / (FN + TN)
 
-## Visualize Traning Set Results
+## Visualize Training Set Results
 #set <- training
-x1 <- seq(min(training[,1]) - 1, max(training[,1]) + 1, by = 0.1) # get the minimum scaled age + go up to the max by increments of .1
-x2 <- seq(min(training[,2]) - 1, max(training[,2]) + 1, by = 0.1) # get the minimum scaled salary + go up to the max by increments of .1
+x1 <- seq(min(training[,1]) - 1, max(training[,1]) + 1, by = 0.05) # get sminimum scaled age + go up to the max by increments of .1
+x2 <- seq(min(training[,2]) - 1, max(training[,2]) + 1, by = 0.05) # get minimum scaled salary + go up to the max by increments of .1
 
-training.grid <- expand.grid(x1,x2) # Create DF from all possible combos of the age + salary vectors
+training.grid <- expand.grid(x1,x2) # Create DF from all possible combos of the age + salary vectors to make a grid
 colnames(training.grid) <- c("Age","EstimatedSalary")
 
-training.probability <- predict(log.reg, newdata = training.grid.set, type = "response") # use grid to predict values w/ our model
+training.probability <- predict(log.reg, newdata = training.grid, type = "response") # use grid to predict values w/ our model
 y.pred.grid <- if_else(training.probability > .5, 1, 0)
 
 plot(training[,-3],
@@ -80,5 +79,47 @@ plot(training[,-3],
 
 contour(x1,x2, matrix(as.numeric(y.pred.grid), length(x1), length(x2)), add = T) # add logistic regression "split" line
 
-points(training.grid, pch = ".", col = if_else(y.pred.grid == 1, "springgreen3", "tomato"))
-points(training, pch = 21, col = if_else(training[,-3] == 1, "green4", "green3"))
+points(training.grid, pch = 19, col = if_else(y.pred.grid == 1, "springgreen3", "tomato"))
+points(training, pch = 19, col = if_else(training[,3] == 1, "green4", "red3"))
+
+'We can see most training set observations who actually made a purchase (green circles) are in the green area (classifier predictions
+of purchased = 1) and most training set observations who did NOT make a purchase (red circles) are in the red region (classifier 
+predictions of purchased = 0)
+
+The *straight, linear* line splitting these 2 regions is our **prediction boundary**. Younger users with lower estimated salaries 
+were less likely to but the SUV and less did. The opposite goes for older users with higher estimated salaries.
+
+The social network can focus their SUV marketing campaign towards users who would fall into the green region as they are predicted
+to be more likely to make a purchase.
+
+Prediction boundary will not be a straight line for non-linear classifiers.
+
+Our logistic regression classifier does a pretty good job of catching the correct users who purchased and those who did not. Incorrect
+predictions are due to the fact that our classifier is linear but our data is *not* linearly distributed.
+
+Now see if the classifier generalizes well to new data.'
+
+## Visualize Test Set Results
+#set <- test
+x1 <- seq(min(test[,1]) - 1, max(test[,1]) + 1, by = 0.05) # get sminimum scaled age + go up to the max by increments of .1
+x2 <- seq(min(test[,2]) - 1, max(test[,2]) + 1, by = 0.05) # get minimum scaled salary + go up to the max by increments of .1
+
+test.grid <- expand.grid(x1,x2) # Create DF from all possible combos of the age + salary vectors
+colnames(test.grid) <- c("Age","EstimatedSalary")
+
+test.probability <- predict(log.reg, newdata = test.grid, type = "response") # use grid to predict values w/ our model
+y.pred.grid <- if_else(test.probability > .5, 1, 0)
+
+plot(test[,-3],
+     main = "Logistic Regression (Test Set)",
+     xlab = "Age",
+     ylab = "Estimated Salary",
+     xlim = range(x1),
+     ylim = range(x2))
+
+contour(x1,x2, matrix(as.numeric(y.pred.grid), length(x1), length(x2)), add = T) # add logistic regression "split" line
+
+points(test.grid, pch = 19, col = if_else(y.pred.grid == 1, "springgreen3", "tomato"))
+points(test, pch = 19, col = if_else(test[,3] == 1, "green4", "red3"))
+
+'Again, the majority of red points are in the red region, and the majority of the green points are in the green region.'
